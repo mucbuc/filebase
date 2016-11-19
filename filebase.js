fc627813b4863a62472d9c227aecb9f28180fa4c
mbusenitz <mbusenitz@gmail.com>
2016-11-19 14:58:58 -0800

    accetable

--- filebase.js
+++ filebase.js
@@ -1,34 +1,87 @@
 #!/usr/bin/env node
 
+'use strict';
+
 const inject = require( 'inject-json' )
   , flatten = require( 'json-flatten' )
   , tempFile = './tmp.json'
-  , fs = require( 'fs' );
+  , fs = require( 'fs' )
+  , path = require( 'path' )
+  , traverse = require( 'traverjs' ); 
 
 function getSources(pathJSON) {
 
-	console.log( 'getSources', pathJSON );
-	console.log( process.cwd() );
+  inject( pathJSON, 'include', (result, next, pathJSON, cb) => {
+    
+    if (!next.hasOwnProperty('sources')) {
+      cb( Object.assign( result, next ) );
+    }
+    else {
+      flatten( next, 'sources' )
+      .then( (src) => {
+        let merged = [];
 
-	inject( pathJSON, 'include' )
-	.then( (result) => {
-		//result = result["inc.json"];
-		console.log( 'inject', result ); 
-		fs.writeFile( tempFile, JSON.stringify( result ), (err) => {
-			if (err) throw err; 
-			flatten( result, 'sources' )
-			.then( (result) => {
-				console.log( 'flattened', result ); 
-			});
+        console.log( 'traverse:', src ); 
 
-			//.catch( (err) => {
-		//		console.log( err ); 
-		//	});
-		}); 
-	})
-	.catch( (err) => {
-		console.log( err ); 
-	});
+        traverse( src, ( file, done ) => {
+
+          console.log( 'file:', pathJSON, file ); 
+          
+          const mergedPath = path.join( path.dirname(pathJSON), file );
+
+          merged.push( mergedPath );
+          done();
+        })
+        .then( () => {
+          cb( Object.assign( result, { "sources": merged } ) );         
+        })
+        .catch( (err) => {
+          console.log( 'error', err );
+        });
+      });
+    }
+  } )
+  .then( (result) => {
+
+    let flattenedResult = { "sources": [] };
+
+    traverse( result, (lib, next) => { 
+      if (lib.hasOwnProperty('sources')) {
+        flattenedResult.sources = flattenedResult.sources.concat( lib.sources );
+      }
+      else {
+        const keys = Object.keys(lib);
+        const key = keys[0];
+        flattenedResult.sources = flattenedResult.sources.concat( lib[key].sources ); 
+      }
+      next();
+    } )
+    .then( () => { 
+
+      console.log( 'result', flattenedResult ); 
+
+      var jsonString = JSON.stringify( result );
+
+      console.log( 'jsonString', jsonString );
+
+      //result = result["inc.json"];
+      
+      fs.writeFile( tempFile, jsonString, (err) => {
+        if (err) throw err; 
+        flatten( result, 'sources' )
+        .then( (result) => {
+          console.log( 'flattened', flattenedResult ); 
+        });
+
+        //.catch( (err) => {
+      //    console.log( err ); 
+      //  });
+      }); 
+    });
+  })
+  .catch( (err) => {
+    console.log( 'error', err ); 
+  });
 
 }
 
