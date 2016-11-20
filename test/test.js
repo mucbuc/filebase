e4be29bc0f27e380f2dbf1b2f22821a4b915c43d
mbusenitz <mbusenitz@gmail.com>
2016-11-20 13:56:30 -0800

    another test

--- test/test.js
+++ test/test.js
@@ -20,3 +20,16 @@ test( 'dummy', (t) => {
 	});
 
 });
+
+test( 'dummy2', (t) => {
+	let e = new Expector( t );
+
+	e.expect( { sources: [ 'test/lib/mod/src/fkjdsa.h', 'test/lib/modB/src/aabbcc.h', 'test/src/main.cpp' ] } ); 
+
+	getSources( './test/test2.json' )
+	.then( (sources) => {
+		e.emit( sources ); 
+		e.check(); 		
+	});
+
+});
