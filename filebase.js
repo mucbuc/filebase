c30ff0619174b8cc879fc941fd89513960b7c467
mbusenitz <mbusenitz@gmail.com>
2017-04-06 18:24:03 -0700

    export list and compose

--- filebase.js
+++ filebase.js
@@ -6,31 +6,40 @@ const program = require( 'commander' )
   , compose = require( './compose' )
   , list = require( './list' );
 
-program
-.version( '0.0.0' )
-.usage( '<json file>' )
-.option('-l, --list', 'list target branches')
-.parse( process.argv );
-
-if (program.args.length != 1) {
-  program.help();
-}
-else if (program.list) {
-  list( program.args[0] )
-  .then( (result) => {
-    console.log( JSON.stringify( result, null, 2 ) );
-  })
-  .catch( (err) => {
-    process.stderr.write( 'error:', err ); 
-  });
+if (module.parent) {
+  module.exports = { 
+    compose, 
+    list
+  };
 }
 else {
-  compose( program.args[0] )
-  .then( (result) => {
-    console.log( JSON.stringify( result, null, 2 ) );
-  })
-  .catch( (err) => {
-    process.stderr.write( 'error:', err ); 
-  });
+
+  program
+  .version( '0.0.0' )
+  .usage( '<json file>' )
+  .option('-l, --list', 'list target branches')
+  .parse( process.argv );
+
+  if (program.args.length != 1) {
+    program.help();
+  }
+  else if (program.list) {
+    list( program.args[0] )
+    .then( (result) => {
+      console.log( JSON.stringify( result, null, 2 ) );
+    })
+    .catch( (err) => {
+      process.stderr.write( 'error:', err ); 
+    });
+  }
+  else {
+    compose( program.args[0] )
+    .then( (result) => {
+      console.log( JSON.stringify( result, null, 2 ) );
+    })
+    .catch( (err) => {
+      process.stderr.write( 'error:', err ); 
+    });
+  }
 }
 
